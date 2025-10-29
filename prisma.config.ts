1e9dc4ff6645699af441dfddc29feaf3bb5af90c
laithha <147397351+laithha@users.noreply.github.com>
2025-10-29 22:35:36 +0200

    fixed some bugs

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import path from "path";
+import dotenv from "dotenv";
+import { defineConfig } from "prisma/config";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  engine: "classic",
+  datasource: {
+    url: process.env.DATABASE_URL!, 
+  },
+});
